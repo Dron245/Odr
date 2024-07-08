eb0e203da3c65ecda0ad76c2d912b0cf5cbc7701
Dron245 <Dron245@yandex.ru>
2024-07-08 21:47:50 +0300

    0807-2147

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,26 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+	document.addEventListener("click", documentActions);
+
+	function documentActions(e) {
+		const targetElement = e.target;
+		console.log(targetElement);
+		if (targetElement.closest('.search__label')) {
+			targetElement.closest('.search').classList.add('_active')
+		} else if (!targetElement.closest('.search') && document.querySelector('.search')) {
+			console.log('123');
+			document.querySelector('.search').classList.remove('_active')
+		}
+	}
+})
+
+window.addEventListener('scroll', function() {
+	if (scrollY > 50) {
+		document.querySelector('header').classList.add('_dark')
+	} else {
+		document.querySelector('header').classList.remove('_dark')
+	}
+})
